e665baade38b26cec1000634b5841d70e34cc4f3
Rifki Okta <rifkiokta105@gmail.com>
2022-09-27 08:38:34 +0700

    update redirect uri

--- src/spotify.js
+++ src/spotify.js
@@ -1,24 +1,26 @@
-import axios from "axios"
+import axios from "axios";
+const prod = window.location.hostname !== "localhost";
+console.log("debug prod", prod);
+const authEndpoint = "https://accounts.spotify.com/authorize?";
+const clientId = "974d47ffdf1d46e0b07180a780d2dc3d";
+const redirectUri = prod
+  ? "https://media-player-six.vercel.app/"
+  : "http://localhost:3000/";
+const scopes = ["user-library-read", "playlist-read-private"];
 
-const authEndpoint = "https://accounts.spotify.com/authorize?"
-const clientId = "974d47ffdf1d46e0b07180a780d2dc3d"
-const redirectUri = "http://localhost:3000/"
-const scopes = ["user-library-read", "playlist-read-private"]
-
-export const loginEndpoint =
-    `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`
-
-
+export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
 
 const apiClient = axios.create({
-    baseURL: "https://api.spotify.com/v1/"
-})
+  baseURL: "https://api.spotify.com/v1/",
+});
 
 export const setClientToken = (token) => {
-    apiClient.interceptors.request.use(async function (config) {
-        config.headers.Authorization = "Bearer " + token
-        return config
-    })
-}
+  apiClient.interceptors.request.use(async function (config) {
+    config.headers.Authorization = "Bearer " + token;
+    return config;
+  });
+};
 
-export default apiClient
+export default apiClient;
